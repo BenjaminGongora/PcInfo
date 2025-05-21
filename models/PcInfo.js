851386c2667a106c872cedb5bb414b3798ea9b2e
Developer Benjamin <benjamingongoradeveloper@gmail.com>
2025-05-21 15:17:43 -0500

    EWCWE

--- models/PcInfo.js
+++ models/PcInfo.js
@@ -1,3 +1,6 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
 const PcInfo = sequelize.define('PcInfo', {
   nombre: {
     type: DataTypes.STRING(100),
